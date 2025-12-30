69cbcc4f407451667eb30c4314c352df913e3056
JimNicolas <jimeneznicolas2318@hotmail.com>
2025-12-30 18:17:54 -0300

    Update Sucursal.jsx

--- src/pages/Sucursal.jsx
+++ src/pages/Sucursal.jsx
@@ -11,7 +11,7 @@ export default function Sucursal() {
       <div className="min-h-screen bg-gray-50">
         <main className="ml-64 min-[1400px]:w-[90%] min-[1400px]:mx-auto">
           <div className="mb-8">
-            <h2 className="text-2xl font-semibold text-gray-900">Solicitud de cambio de sucursal</h2>
+            <h2 className="text-2xl text-gray-900">Solicitud de cambio de sucursal</h2>
             <p className="text-gray-600">Solicita el cambio de tu sucursal de adscripción para futuras entregas y
               gestiones</p>
           </div>
